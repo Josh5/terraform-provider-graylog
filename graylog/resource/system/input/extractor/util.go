3d23ec9f5b3c1cbfc8e627c8b87e81e846b84ad1
Josh.5 <jsunnex@gmail.com>
2024-07-23 14:19:38 +1200

    Add some debug logging to file

--- graylog/resource/system/input/extractor/util.go
+++ graylog/resource/system/input/extractor/util.go
@@ -2,6 +2,8 @@ package extractor
 
 import (
 	"encoding/json"
+	"log"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
@@ -19,57 +21,80 @@ const (
 	keyType            = "type"
 )
 
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
 func getDataFromResourceData(d *schema.ResourceData) (map[string]interface{}, error) {
+	log.Println("[DEBUG] Entering getDataFromResourceData")
+
 	data, err := convert.GetFromResourceData(d, Resource())
 	if err != nil {
+		log.Printf("[ERROR] Error converting resource data: %s", err)
 		return nil, err
 	}
+	log.Printf("[DEBUG] Data after conversion: %+v", data)
 	util.RenameKey(data, "type", "extractor_type")
 	util.SetDefaultValue(data, "target_field", "")
 	util.SetDefaultValue(data, "condition_value", "")
 
 	if err := convert.JSONToData(data, keyExtractorConfig); err != nil {
+		log.Printf("[ERROR] Error converting JSON to data: %s", err)
 		return nil, err
 	}
 	util.RenameKey(data, keyExtractorID, keyID)
 
+	log.Printf("[DEBUG] Data before handling converters: %+v", data)
 	if conv, ok := data[keyConverters].([]interface{}); ok {
 		for i, c := range conv {
 			converter := c.(map[string]interface{})
 			if err := convert.JSONToData(converter, keyConfig); err != nil {
+				log.Printf("[ERROR] Error converting JSON to data for converters: %s", err)
 				return nil, err
 			}
 			conv[i] = converter
 		}
 		data[keyConverters] = conv
 	}
-	
+
+	log.Printf("[DEBUG] Final data: %+v", data)
 	return data, nil
 }
 
 func setDataToResourceData(d *schema.ResourceData, data map[string]interface{}) error {
+	log.Println("[DEBUG] Entering setDataToResourceData")
+	log.Printf("[DEBUG] Data before converting to JSON: %+v", data)
+
 	if err := convert.DataToJSON(data, keyExtractorConfig); err != nil {
+		log.Printf("[ERROR] Error converting data to JSON: %s", err)
 		return err
 	}
 	util.RenameKey(data, keyID, keyExtractorID)
 
-	converters := data[keyConverters].([]interface{})
-	for i, a := range converters {
-		elem := a.(map[string]interface{})
-		b, err := json.Marshal(elem[keyConfig])
-		if err != nil {
-			return err
+	log.Printf("[DEBUG] Data after converting to JSON: %+v", data)
+	if conv, ok := data[keyConverters].([]interface{}); ok {
+		for i, c := range conv {
+			converter := c.(map[string]interface{})
+			if b, err := json.Marshal(converter[keyConfig]); err != nil {
+				log.Printf("[ERROR] Error marshaling JSON for converters: %s", err)
+				return err
+			} else {
+				converter[keyConfig] = string(b)
+			}
+			conv[i] = converter
 		}
-		elem[keyConfig] = string(b)
-		converters[i] = elem
+		data[keyConverters] = conv
 	}
 
-	data[keyConverters] = converters
+	log.Printf("[DEBUG] Final data before setting resource: %+v", data)
 
 	if err := convert.SetResourceData(d, Resource(), data); err != nil {
+		log.Printf("[ERROR] Error setting resource data: %s", err)
 		return err
 	}
 
 	d.SetId(d.Get(keyInputID).(string) + "/" + d.Get(keyExtractorID).(string))
+	log.Printf("[DEBUG] Resource ID set to: %s", d.Id())
 	return nil
 }
